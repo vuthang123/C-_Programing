d2c2fc8ecf2f5414aae840f02445bf4923f5300c
ThangVh3 <hungthang12b2@gmail.com>
2024-05-12 15:28:41 +0700

    update code

--- day1/main.cc
+++ day1/main.cc
@@ -1,12 +1,15 @@
-
-/* Note need debug*/
 #include <iostream>
 using namespace std;
 int main()
 {
+    /* Note need debug*/
     int amount = 100;
     amount += amount * 2;
     cout << amount << endl;
-
+    /* Note need debug*/
+    int x= 5;
+    int y= -2;
+    int z = 2;
+    cout << (x + y * z <= x + z * z - x) << endl;
     return 0;
 }
